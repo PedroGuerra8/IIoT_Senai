8fba07ca4435524a87894f1ae151bad172a27b6a
Pedro Guerra <pguerra872@gmail.com>
2025-02-20 22:23:24 -0300

    up
    20.02

---
+++ DOMOTICA/domotica/src/models/devices.ts
@@ -0,0 +1,29 @@
+export class Device {
+    nome: string = "";
+    state: boolean = false;
+    color: string = "#cca947";
+}
+
+export class Places {
+    nome: string = "";
+    devices: Array<Device> = [];
+}
+
+
+
+
+
+
+
+
+
+/*export interface PlacesInterface {
+    nome: string,
+    devices: Array<Device>,
+}
+
+
+export class PlacesClass {
+    nome: string = "";
+    devices: Array<Device> = [];
+}*/
